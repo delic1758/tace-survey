a5a009874d82f9e6080547f0ff48778a769fdb35
SK0227 <131940351+delic1758@users.noreply.github.com>
2026-07-13 15:25:46 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+// Google Apps Script 배포 후 발급되는 웹앱 URL을 아래에 붙여넣으세요.
+const CONFIG = { SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx7ok39CtKq2_T3SGenyNOR3Uydy59fkez_Tzyftwhly3G-Am4h9ZWGPS0JmqBJzhLwvw/exec" };
